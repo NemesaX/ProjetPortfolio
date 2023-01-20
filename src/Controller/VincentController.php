f036428233a3caa51f84593b54fc147f62c849fd
NemesaX <leovinc74@gmail.com>
2023-01-20 16:31:07 +0100

    Update VincentController.php

--- src/Controller/VincentController.php
+++ src/Controller/VincentController.php
@@ -9,9 +9,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class VincentController extends AbstractController
 {
     #[Route('/site', name: 'app_site')]
-    public function index(): Response
+    public function site(): Response
     {
-        return $this->render('site/index.html.twig', [
+        return $this->render('vincent/index.html.twig', [
+            'controller_name' => 'VincentController',
+        ]);
+    }
+
+    #[Route('/Accueil', name: 'app_Accueil')]
+    public function Accueil(): Response
+    {
+        return $this->render('vincent/index.html.twig', [
+            'controller_name' => 'VincentController',
+        ]);
+    }
+
+    #[Route('/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('vincent/cv.html.twig', [
+            'controller_name' => 'VincentController',
+        ]);
+    }
+
+    #[Route('/Blog', name: 'app_Blog')]
+    public function Blog(): Response
+    {
+        return $this->render('vincent/Blog.html.twig', [
+            'controller_name' => 'VincentController',
+        ]);
+    }
+
+    #[Route('/Portfolio', name: 'app_Portfolio')]
+    public function Portfolio(): Response
+    {
+        return $this->render('vincent/Portfolio.html.twig', [
             'controller_name' => 'VincentController',
         ]);
     }
